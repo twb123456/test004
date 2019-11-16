df8fa1a5f743805298eb1345d4e30ba6a3df3043
zgd <zgd@.com>
2019-11-16 20:23:34 +0800

    update user.class

--- src/main/java/com/chuangzhi/pojo/User.java
+++ src/main/java/com/chuangzhi/pojo/User.java
@@ -4,6 +4,7 @@ public class User {
     private String username;
     private String password;
     private String brithday;
+    private String age;
 
     public String getUsername() {
         return username;
